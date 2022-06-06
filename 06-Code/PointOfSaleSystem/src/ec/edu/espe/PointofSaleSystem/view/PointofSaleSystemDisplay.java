17472529d25bd690b40cfb318e3193d54b9aa6cf
MateoCondor <96481656+MateoCondor@users.noreply.github.com>
2022-06-05 23:42:52 -0500

    PointOfSaleSystem V1.2.5

--- 06-Code/PointOfSaleSystem/src/ec/edu/espe/PointofSaleSystem/view/PointofSaleSystemDisplay.java
+++ 06-Code/PointOfSaleSystem/src/ec/edu/espe/PointofSaleSystem/view/PointofSaleSystemDisplay.java
@@ -10,56 +10,72 @@ import java.util.Scanner;
  */
 public class PointofSaleSystemDisplay {
     public static void main(String[] args) {
-        System.out.println("This is the Point of Sale System program");
-        
         ArrayList<Customer> customers;
         Scanner imput;
         imput = new Scanner(System.in);
+        int returnMenu = 0;
         
-        System.out.println("\n|====|MENU|====|");
-        System.out.println("-1- Create Invoice");
-        System.out.println("-2- Sales register");
-        System.out.println("-3- Register new product");
-        System.out.println("-4- Search product ");
-        System.out.println("-5- Payments and expenses");
-        System.out.println("-6- Daily Report");
-        System.out.println("\n|Insert your choise please|\n");
-        int menuOption = imput.nextInt();
+        do{
+            System.out.println("This is the Point of Sale System program");
+
+            System.out.println("\n|====|MENU|====|");
+            System.out.println("-1- Create Invoice");
+            System.out.println("-2- Sales register");
+            System.out.println("-3- Register new product");
+            System.out.println("-4- Search product ");
+            System.out.println("-5- Payments and expenses");
+            System.out.println("-6- Daily Report");
+            System.out.println("\n|Insert your choise please|\n");
+            int menuOption = imput.nextInt();
+
+            switch(menuOption){
+                case 1 :
+                    System.out.println("\nPlease enter the following data");
+                    System.out.println("Customer name: ");
+                    String customerName = imput.next();
+                    System.out.println("Identification card: ");
+                    int customerID = imput.nextInt();
+                    System.out.println("Phone: ");
+                    int customerPhone = imput.nextInt();
+                    System.out.println("Address: ");
+                    String customerAdrress = imput.next();
+
+                    customers = new ArrayList<>();
+                    customers.add(0,new Customer(customerName, customerID, customerPhone, customerAdrress));
+
+                    System.out.println("Invoice data -> " + customers);
+
+                    System.out.println("Desea regresar al menu? \n-1- Si \n-0- No,salir.");
+                    returnMenu = imput.nextInt();
+                    break;
+                case 2 :
+                    System.out.println("\nSales made by date and time will be recorded\n");
+                    System.out.println("Desea regresar al menu? \n-1- Si \n-0- No,salir.");
+                    returnMenu = imput.nextInt();
+                    break;
+                case 3 :
+                    System.out.println("\nA new product will be registered with a name and a unit price\n");
+                    System.out.println("Desea regresar al menu? \n-1- Si \n-0- No,salir.");
+                    returnMenu = imput.nextInt();
+                    break;
+                case 4 :
+                    System.out.println("\nSystem to search for the existence and price of any previously registered product\n");
+                    System.out.println("Desea regresar al menu? \n-1- Si \n-0- No,salir.");
+                    returnMenu = imput.nextInt();
+                    break;
+                case 5 :
+                    System.out.println("\nPayments to suppliers and expenses for basic services will be recorded\n");
+                    System.out.println("Desea regresar al menu? \n-1- Si \n-0- No,salir.");
+                    returnMenu = imput.nextInt();
+                    break;
+                case 6 :
+                    System.out.println("\nRecord of daily sales, payments and expenses organized by date and time\n");
+                    System.out.println("Desea regresar al menu? \n-1- Si \n-0- No,salir.");
+                    returnMenu = imput.nextInt();
+                    break;
+                }
+        }while(returnMenu==1);
         
-        switch(menuOption){
-            case 1 :
-                System.out.println("\nPlease enter the following data");
-                System.out.println("Customer name: ");
-                String customerName = imput.next();
-                System.out.println("Identification card: ");
-                int customerID = imput.nextInt();
-                System.out.println("Phone: ");
-                int customerPhone = imput.nextInt();
-                System.out.println("Address: ");
-                String customerAdrress = imput.next();
-                
-                customers = new ArrayList<>();
-                customers.add(0,new Customer(customerName, customerID, customerPhone, customerAdrress));
-                
-                System.out.println("Invoice data -> " + customers);
-                       
-                imput.nextInt();
-            case 2 :
-                System.out.println("\nSales made by date and time will be recorded\n");
-                imput.nextInt();
-            case 3 :
-                System.out.println("\nA new product will be registered with a name and a unit price\n");
-                imput.nextInt();
-            case 4 :
-                System.out.println("\nSystem to search for the existence and price of any previously registered product\n");
-                imput.nextInt();
-            case 5 :
-                System.out.println("\nPayments to suppliers and expenses for basic services will be recorded\n");
-                imput.nextInt();;
-            case 6 :
-                System.out.println("\nRecord of daily sales, payments and expenses organized by date and time\n");
-                imput.nextInt();
-        }
         
     }
 
